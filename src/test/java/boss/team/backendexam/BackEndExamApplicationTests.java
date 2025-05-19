27f2abaeee3bbc877c29e16dac8d981375bbe255
TheBOSS <hossamhaida4@gmail.com>
2025-05-19 08:55:04 +0100

    initial commit

---
+++ src/test/java/boss/team/backendexam/BackEndExamApplicationTests.java
@@ -0,0 +1,13 @@
+package boss.team.backendexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
